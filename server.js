2cbb169d8b9e51375037f2ca9cfd07edb9519abc
lirla@gmail.com <firexable@gmail.com>
2016-01-08 18:33:21 +0300

    Use grapiql on root route

--- server.js
+++ server.js
@@ -1,27 +1,10 @@
 import express from 'express'
-import {getUser} from "./api/vk"
 import graphqlHTTP from "express-graphql"
 import {Schema} from "./schemas/vk"
-import {graphql} from "graphql"
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    let query = `{
-            user(uid:91035623){
-                last_name
-                photo_50
-                friends(limit:3){
-                    first_name
-                    last_name
-                 }
-                }
-            }`;
-    graphql(Schema,query).then(result =>{
-        res.send(result['data']);
-    });
-});
-
+app.use('/', graphqlHTTP({ schema: Schema, graphiql: true }));
 
 app.listen(3000,()=>{
     console.log("App started at port 3000");
